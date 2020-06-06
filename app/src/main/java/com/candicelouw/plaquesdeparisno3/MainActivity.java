d36d7e45c6e8e00054e034217d5bb911d553d82d
CandiceLouw <canlouw@gmail.com>
2020-06-06 13:38:27 +0200

    Update app display name and Manifest file entries.

--- app/src/main/java/com/candicelouw/plaquesdeparisno3/MainActivity.java
+++ app/src/main/java/com/candicelouw/plaquesdeparisno3/MainActivity.java
@@ -2,6 +2,7 @@ package com.candicelouw.plaquesdeparisno3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 /**
  * Authored by: Candice Louw 20200606
@@ -9,10 +10,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
