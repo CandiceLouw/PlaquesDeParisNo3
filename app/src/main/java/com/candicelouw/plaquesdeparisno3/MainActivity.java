13c94496e5c6f6411bd232b7662933ecf5d7c97c
CandiceLouw <canlouw@gmail.com>
2020-06-06 15:42:30 +0200

    Include WebView component.

--- app/src/main/java/com/candicelouw/plaquesdeparisno3/MainActivity.java
+++ app/src/main/java/com/candicelouw/plaquesdeparisno3/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        myWebView.loadUrl("file:///android_asset/plaquesdeparisno3.html");
     }
 }
