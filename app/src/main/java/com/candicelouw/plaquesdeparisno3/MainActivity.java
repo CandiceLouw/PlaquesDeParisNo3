0aed98d8dad2fd433a9b2f1de1a31e848d013bf0
CandiceLouw <canlouw@gmail.com>
2020-06-06 13:34:36 +0200

    Update author information.

--- app/src/main/java/com/candicelouw/plaquesdeparisno3/MainActivity.java
+++ app/src/main/java/com/candicelouw/plaquesdeparisno3/MainActivity.java
@@ -3,7 +3,12 @@ package com.candicelouw.plaquesdeparisno3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Authored by: Candice Louw 20200606
+ */
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
